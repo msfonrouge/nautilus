569cd1c4f4083835ef514ff766c37a1d1b867498
santiago lopez saubidet <slopez95@hotmail.com>
2023-06-06 17:23:21 -0300

    MVC de los 3 modelos (-Front)

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,56 @@
+class BookingsController < ApplicationController
+  before_action :set_bookings, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+    @yacht = Yacht.find(params[:yacht_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @yacht = Yacht.find(params[:yacht_id])
+    @booking.yacht_id = @yacht.id
+    @booking.user_id = current_user.id
+    if @booking.save
+      redirect_to booking_path(@booking.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    @booking.yacht = @yacht
+    if @booking.save
+      redirect_to yacht_booking_path(@booking.id),
+                  notice: "El booking se ha actualizado perfectamente"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def set_bookings
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :status, :yacht_id, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :yachts
+  resources :yachts do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, except: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
