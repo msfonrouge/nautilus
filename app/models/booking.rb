58b64097ab9924281a0b677822e36335567c6c56
santiago lopez saubidet <slopez95@hotmail.com>
2023-06-09 00:26:20 -0300

    correcciones de bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,12 @@
 class Booking < ApplicationRecord
+  after_initialize :set_defaults
+
   belongs_to :user
   belongs_to :yacht
   validates :date, :status, presence: true
-  validates :status, inclusion: { in: ["available", "not-available"]}
+  validates :status, inclusion: { in: ["available", "not-available"] }
 
+  def set_defaults
+    self.status = "not-available"
+  end
 end
